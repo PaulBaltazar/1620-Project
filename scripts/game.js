d45a2d889048a222efdd7e98c0691de4104b948f
Matthew <matthewjameslew@gmail.com>
2025-04-01 14:13:37 -0700

    changed jpeg to png

--- scripts/game.js
+++ scripts/game.js
@@ -25,20 +25,20 @@ class Cards {
 
 
 const cards = [];
-cards[0] = new Cards('./images/boo.jpeg', 'boo', 1);
-cards[1] = new Cards('./images/boo.jpeg', 'boo', 1);
-cards[2] = new Cards('./images/Luigi.jpeg', 'Luigi', 2);
-cards[3] = new Cards('./images/Luigi.jpeg', 'Luigi', 2);
-cards[4] = new Cards('./images/Mario.jpeg', 'Mario', 3);
-cards[5] = new Cards('./images/Mario.jpeg', 'Mario', 3);
-cards[6] = new Cards('./images/Peach.jpeg', 'Peach', 4);
-cards[7] = new Cards('./images/Peach.jpeg', 'Peach', 4);
-cards[8] = new Cards('./images/Toad.jpeg', 'Toad', 5);
-cards[9] = new Cards('./images/Toad.jpeg', 'Toad', 5);
-cards[10] = new Cards('./images/Waluigi.jpeg', 'Waluigi', 6);
-cards[11] = new Cards('./images/Waluigi.jpeg', 'Waluigi', 6);
-cards[12] = new Cards('./images/Yoshi.jpeg', 'Yoshi', 7);
-cards[13] = new Cards('./images/Yoshi.jpeg', 'Yoshi', 7);
+cards[0] = new Cards('./images/boo.png', 'boo', 1);
+cards[1] = new Cards('./images/boo.png', 'boo', 1);
+cards[2] = new Cards('./images/Luigi.png', 'Luigi', 2);
+cards[3] = new Cards('./images/Luigi.png', 'Luigi', 2);
+cards[4] = new Cards('./images/Mario.png', 'Mario', 3);
+cards[5] = new Cards('./images/Mario.png', 'Mario', 3);
+cards[6] = new Cards('./images/Peach.png', 'Peach', 4);
+cards[7] = new Cards('./images/Peach.png', 'Peach', 4);
+cards[8] = new Cards('./images/Toad.png', 'Toad', 5);
+cards[9] = new Cards('./images/Toad.png', 'Toad', 5);
+cards[10] = new Cards('./images/Waluigi.png', 'Waluigi', 6);
+cards[11] = new Cards('./images/Waluigi.png', 'Waluigi', 6);
+cards[12] = new Cards('./images/Yoshi.png', 'Yoshi', 7);
+cards[13] = new Cards('./images/Yoshi.png', 'Yoshi', 7);
 
 
 
